a06b758d0b30c9b00ddf50602a84c54cce250980
Tobiasz Fic <tobi.fic@gmail.com>
2023-05-22 14:00:25 +0200

    code cleanup

--- cake_start.py
+++ cake_start.py
@@ -4,18 +4,14 @@ import re
 
 
 class CakeClient:
-    def __init__(self, intermediate_addrs: list, destination_addr: str):
-        # list of nodes on the way to the destination
-        self.node_address_list = intermediate_addrs
-        self.node_address_list.append(destination_addr)
-        self.message = ''
+    def __init__(self, node_address_list: list, logging=False):
+        self.node_address_list = node_address_list
+        self.logging = logging
 
     def __construct_cake(self, message_body) -> str:
-        cake = ''
-        for i in range(1, len(self.node_address_list)):
-            cake += self.node_address_list[i] + ';'
-        cake += message_body
-        return cake
+        cake_list = list(self.node_address_list[1:])
+        cake_list.append(message_body)
+        return ';'.join(cake_list)
 
     @staticmethod
     def __parse_udp_address(udp_address):
@@ -32,27 +28,31 @@ class CakeClient:
         else:
             return None
 
+    def __log_send_message(self, ip: str, port: int, message_body: str, cake: str) -> None:
+        if (not self.logging):
+            return
+        log = "UDP target IP:" + ip + '\n'
+        log += "UDP target port" + str(port) + '\n'
+        log += "message:" + message_body + '\n'
+        log += "cake:" + cake
+        print(log)
+
     def send_message(self, message_body):
         cake = self.__construct_cake(message_body)
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
         ip, port = self.__parse_udp_address(self.node_address_list[0])
         sock.sendto(bytes(cake, "ascii"), (ip, port))
-
-        print("UDP target IP:", ip)
-        print("UDP target port", port)
-        print("message:", message_body)
-        print("cake:", cake)
+        self.__log_send_message(ip, port, message_body, cake)
 
     def await_message(self) -> str:
         sock = socket.socket(socket.AF_INET,
                              socket.SOCK_DGRAM)
         sock.bind(("localhost", 31337))
-
         while True:
             data, addr = sock.recvfrom(1024)
             print("received message: %s" % data)
 
 
-cc = CakeClient([], '192.168.132.40:31337')
+cc = CakeClient(['192.168.132.40:31337'], logging=True)
 cc.send_message('helo')
 cc.await_message()
